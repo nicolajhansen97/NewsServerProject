918433e2b09b7b417a5c794c4dd55402037e424d
Nicolaj Hansen <nicolajhansen97@live.dk>
2021-10-25 15:20:04 +0200

    test

--- MVVMStart/View/ExampleView.xaml.cs
+++ MVVMStart/View/ExampleView.xaml.cs
@@ -21,5 +21,10 @@ namespace MVVMStart.View
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
